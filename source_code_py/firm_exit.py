2a1620f39970385b3b50897871f37aaf7a03b75e
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-11-07 12:50:51 +0530

    Python Code: firm_exit

---
+++ source_code_py/firm_exit.py
@@ -0,0 +1,105 @@
+"""
+Firm valuation with exit option.
+
+"""
+
+from quantecon.markov import tauchen
+import numpy as np
+from collections import namedtuple
+from s_approx import successive_approx
+
+
+# NamedTuple Model
+Model = namedtuple("Model", ("n", "z_vals", "Q", "β", "s"))
+
+
+def create_exit_model(
+        n=200,                  # productivity grid size
+        ρ=0.95, μ=0.1, ν=0.1,   # persistence, mean and volatility
+        β=0.98, s=100.0         # discount factor and scrap value
+    ):
+    """
+    Creates an instance of the firm exit model.
+    """
+    mc = tauchen(ρ, ν, b=μ, n=n)
+    z_vals, Q = mc.state_values, mc.P
+    return Model(n=n, z_vals=z_vals, Q=Q, β=β, s=s)
+
+
+def no_exit_value(model):
+    """Compute value of firm without exit option."""
+    n, z_vals, Q, β, s = model
+    I = np.identity(n)
+    return np.linalg.solve((I - β * Q), z_vals)
+
+
+def T(v, model):
+    """The Bellman operator Tv = max{s, π + β Q v}."""
+    n, z_vals, Q, β, s = model
+    h = z_vals + β * np.dot(Q, v)
+    return np.maximum(s, h)
+
+
+def get_greedy(v, model):
+    """Get a v-greedy policy."""
+    n, z_vals, Q, β, s = model
+    σ = s >= z_vals + β * np.dot(Q, v)
+    return σ
+
+
+def vfi(model):
+    """Solve by VFI."""
+    v_init = no_exit_value(model)
+    v_star = successive_approx(lambda v: T(v, model), v_init)
+    σ_star = get_greedy(v_star, model)
+    return v_star, σ_star
+
+
+# Plots
+
+
+import matplotlib.pyplot as plt
+
+def plot_val(savefig=False,
+             figname="../figures/firm_exit_1.pdf"):
+
+    fig, ax = plt.subplots(figsize=(9, 5.2))
+
+    model = create_exit_model()
+    n, z_vals, Q, β, s = model
+
+    v_star, σ_star = vfi(model)
+    h = z_vals + β * np.dot(Q, v_star)
+
+    ax.plot(z_vals, h, "-", linewidth=3, alpha=0.6, label=r"$h^*$")
+    ax.plot(z_vals, s * np.ones(n), "-", linewidth=3, alpha=0.6, label=r"$s$")
+    ax.plot(z_vals, v_star, "k--", linewidth=1.5, alpha=0.8, label=r"$v^*$")
+
+    ax.legend(frameon=False)
+    ax.set_xlabel(r"$z$")
+
+    plt.show()
+    if savefig:
+        fig.savefig(figname)
+
+
+def plot_comparison(savefig=False,
+                    figname="../figures/firm_exit_2.pdf"):
+
+    fig, ax = plt.subplots(figsize=(9, 5.2))
+
+    model = create_exit_model()
+    n, z_vals, Q, β, s = model
+
+    v_star, σ_star = vfi(model)
+    w = no_exit_value(model)
+
+    ax.plot(z_vals, v_star, "k-", linewidth=2, alpha=0.6, label="$v^*$")
+    ax.plot(z_vals, w, linewidth=2, alpha=0.6, label="no-exit value")
+
+    ax.legend(frameon=False)
+    ax.set_xlabel(r"$z$")
+
+    plt.show()
+    if savefig:
+        fig.savefig(figname)
